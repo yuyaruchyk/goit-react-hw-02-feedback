37f30b208a6842259ded58e9fa0d941f20dd8732
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-03 11:12:06 +0100

    Create App.styled.js

---
+++ src/components/App.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+  background-color: white;
+  width: 750px;
+  height: 750px;
+  margin-left: auto;
+  margin-right: auto;
+  color: black;
+`;
