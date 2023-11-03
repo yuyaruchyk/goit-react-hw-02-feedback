dfa517a3e37fca380207bf83aaa26ceade280d4b
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-03 11:24:53 +0100

    upd
    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions"
-import { Statistic } from "./Statistic/Statistic";
-import { Section } from "./Section/Section";
-import { Notification } from "./Notification message/Notification message";
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistic } from './Statistic/Statistic';
+import { Section } from './Section/Section';
+import { Notification } from './Notification message/Notification message';
+import { Container } from './App.styled';
 
+export class App extends Component{
+  static propTypes = {};
 
-class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-  updateState = newState => {
-    this.setState({
-      ...newState,
-    });
+  onBtnClick = actionType => {
+    this.setState(prevState => ({
+      [actionType]: prevState[actionType] + 1,
+    }));
   };
 
   countTotalFeedback = () => {
     const total = Object.values(this.state).reduce((prev, nmbr) => prev + nmbr);
     return total;
   };
-
   countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const positive = Math.floor((this.state.good * 100) / total);
+    const positive = Math.floor(
+      (this.state.good * 100) / this.countTotalFeedback()
+    );
     return positive;
   };
 
@@ -33,10 +35,10 @@ class App extends Component {
     const total = this.countTotalFeedback();
 
     return (
-      <div>
+      <Container>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            onBtnClick={this.updateState} 
+            onBtnClick={this.onBtnClick}
             arrayOfOptions={Object.keys(this.state)}
           />
         </Section>
@@ -44,19 +46,17 @@ class App extends Component {
         {total ? (
           <Section title="Statistics">
             <Statistic
-              rates={this.state} 
-              total={total}
+              rates={this.state}
+              total={this.countTotalFeedback()}
               positive={this.countPositiveFeedbackPercentage()}
             />
           </Section>
         ) : (
           <Notification message="There is no feedback" />
         )}
-      </div>
+      </Container>
     );
   }
 }
 
 export default App;
-
-
