7775a6d31068cac3db49fe72dbeb6e1cd2652139
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-03 13:31:07 +0100

    upd

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -6,22 +6,30 @@ display: flex;
 justify-content: space-evenly;
 `
 export const ControlsHeader = styled.h2`
-font-size: 36px;
-font-weight: 700;
+ font-size: 30px;
+  font-weight: 700;
+  text-transform: capitalize;
+  text-shadow: 0 1px 1px #2d2020;
 `
 export const Btn = styled.button`
-padding: 6px 15px;
-font-size: 18px;
-text-transform: capitalize;
-background-color: transparent;
-border: solid 1px silver;
-border-radius: 5px;
-transition: all .07s ease-out;
-&:hover {
-    background-color: silver;
-  }
-&:active {
-    background-color: #415BD2;
-    color: white;
+  margin-bottom: 20px;
+  border: none;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.5;
+  letter-spacing: 0.04em;
+  cursor: pointer;
+
+  display: block;
+  min-width: 169px;
+  height: 40px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+
+  color: white;
+  background-color: #7c90f3;
+  margin-left: auto;
+  margin-right: auto;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
   }
 `
